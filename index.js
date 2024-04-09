123b6d5a2621a07e4962bd5791f639f15cae2356
rggamiao <rejicamiling@gmail.com>
2024-04-08 17:43:39 -0700

    seccond commit

--- index.js
+++ index.js
@@ -1,73 +1,32 @@
-let element = newImage('assets/red-character/static.gif')
-element.style.zIndex = 1;
 
-let direction = null;
+const pc = newPlayableCharacter(100, 110)
 
-function moveCharacter() {
-    if (direction === 'west') {
-        x -= 1
-    }
-    if (direction === 'north') {
-        y += 1
-    }
-    if (direction === 'east') {
-        x += 1
-    }
-    if (direction === 'south') {
-        y -= 1
-    }
-    element.style.left = x + 'px'
-    element.style.bottom = y + 'px'
+const npc = newNonPlayableCharacter(50, 300)
+
+
+
+async function moveNPC(){
+    await npc.walkNorth(1400)
+    await npc.walkEast(1200)
+    await npc.walkSouth(300)
+    await npc.walkEast(1500)
+    await npc.walkSouth(1500)
+    await npc.walkWest(2700)
+    await npc.walkNorth(400)
 }
 
-setInterval(moveCharacter, 1)
-
-function walkEast() {
-    direction = 'east'
-    element.src = ./assets/red-character/east.gif
-}
-
-function walkNorth() {
-    direction = 'north'
-    element.src = ./assets/red-character/north.gif
-}
-
-function walkWest() {
-    direction = 'west'
-    element.src = ./assets/red-character/west.gif
-}
-
-function walkSouth() {
-    direction = 'south'
-    element.src = ./assets/red-character/south.gif
-}
-
-function stop() {
-    direction = null
-    element.src = ./assets/red-character/static.gif
-}
-
-return {
-    element: element,
-    walkWest: walkWest,
-    walkNorth: walkNorth,
-    walkEast: walkEast,
-    walkSouth: walkSouth,
-    stop: stop
-}
-}
-
-function sleep(time) {
-return new Promise (resolve => {
-    setTimeout(resolve, time)
-});
-}
-
-async function walkEast(time) {
-direction = 'east';
-element.src = ./assets/red-character/east.gif;
-await sleep(time);
-stop();
-}
+moveNPC()
 
 
+const inventory = newInventory()
+move(inventory).to(0, 0)
+
+
+move(newImage('assets/tree.png')).to(200, 450)
+move(newImage('assets/pillar.png')).to(350, 250)
+move(newImage('assets/pine-tree.png')).to(450, 350)
+move(newImage('assets/crate.png')).to(150, 350)
+move(newImage('assets/well.png')).to(500, 575)
+move(newItem('assets/sword.png')).to(500, 555)
+move(newItem('assets/shield.png')).to(165, 335)
+move(newItem('assets/staff.png')).to(600, 250)
